d5de9bde2e73b03aa502d57790b8c9c3e00d80c8
jnthnmyrs <jonathan.andrew.myers@gmail.com>
2025-01-25 00:06:41 -0700

    keep the majesty, ditch the others

--- components/LandingPageElements.tsx
+++ components/LandingPageElements.tsx
@@ -5,26 +5,8 @@ export default function LandingPageElements() {
   return (
 
     <div className=" absolute hidden lg:block top-0 left-0 w-full h-full pointer-events-none">
-        <Image
-          src="/landing-page/element-03.svg"
-          alt="decorative element"
-          width={100}
-          height={100}
-          className="absolute bottom-[10%] left-[5%] w-16 md:w-24 lg:w-32 "
-        />
-    
-      
-        <Image
-          src="/landing-page/element-04.svg"
-          alt="decorative element"
-          width={100}
-          height={100}
-          className="absolute bottom-[10%] right-[8%] w-20 md:w-28 lg:w-36 "
-        />
-      
-      {/* ----- */}
-      {/* ----- */}
-      {/* ----- */}
+
+
 
         <Image
           src="/landing-page/element-06.svg"
